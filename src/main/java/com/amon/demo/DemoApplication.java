acdefdf6cd97f700a8abc0342e302c4d5b836018
asabul@mobileaccord.com <asabul@mobileaccord.com>
2022-05-16 19:50:44 +0300

    project with sample screen shot of how it looks

--- src/main/java/com/amon/demo/DemoApplication.java
+++ src/main/java/com/amon/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication application = new SpringApplication(DemoApplication.class);
-//declare the properties including the caching property for thymeleaf
+        //declare the properties including the caching property for thymeleaf
         Properties properties = new Properties();
         properties.setProperty("spring.cache.type", "NONE");
         application.setDefaultProperties(properties);
--- src/test/java/com/amon/demo/DemoApplicationTests.java
+++ src/test/java/com/amon/demo/DemoApplicationTests.java
@@ -2,9 +2,12 @@ package com.amon.demo;
 
 import com.amon.demo.model.CustomerEntity;
 import com.amon.demo.service.CustomerService;
+import com.amon.demo.web.CustomerMvcController;
+import java.util.HashMap;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +25,20 @@ public class DemoApplicationTests {
     public void contextLoads() {
     }
 
+    //test the findAll customers function
     @Test
-    public void testFindAll_thenStudentListShouldBeReturned() {
+    public void testFindAll() {
         List<CustomerEntity> foundCustomers = service.getAllCustomers();
         assertNotNull(foundCustomers);
         assertEquals(41, foundCustomers.size());
+    }
+    //test that read regex works and able to pick from the file regex.txt
+    @Test
+    public void testRegex() {
+        HashMap<String, String> map = new HashMap<>();
+        CustomerMvcController controller = new CustomerMvcController();
+        map = controller.ReadRegex();
+        assertNotNull(map);
+        assertTrue(map.containsKey("256"));
     }
 }
